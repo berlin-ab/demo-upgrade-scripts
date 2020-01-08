cc068dd1756b3af2ad5db95c29020627cb0512b6
Adam Berlin <aberlin@pivotal.io>
2020-01-08 15:28:40 -0500

    Add flags necessary to upgrade tablespaces.

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,4 @@
+set -e
 source ./scripts/gpdb5-environment.sh
 source ./gpdb5-installation/greenplum_path.sh
 
@@ -13,12 +14,14 @@ gpstop -a
 psql --version
 
 
-pg_upgrade --link \
+pg_upgrade  \
 	   --old-bindir=./gpdb5-installation/bin \
 	   --new-bindir=./gpdb6-installation/bin \
 	   --old-datadir=./gpdb5-data/qddir/demoDataDir-1 \
 	   --new-datadir=./gpdb6-data/qddir/demoDataDir-1 \
-	   --mode=dispatcher
+	   --mode=dispatcher \
+	   --old-gp-dbid=1 \
+	   --new-gp-dbid=1
 
 rsync_excludes="--exclude=internal.auto.conf --exclude=postgresql.conf --exclude=pg_hba.conf --exclude=postmaster.opts --exclude=postgresql.auto.conf"
 
@@ -26,24 +29,33 @@ rsync -a --delete $rsync_excludes gpdb6-data/qddir/demoDataDir-1/ gpdb6-data/dbf
 rsync -a --delete $rsync_excludes gpdb6-data/qddir/demoDataDir-1/ gpdb6-data/dbfast2/demoDataDir1
 rsync -a --delete $rsync_excludes gpdb6-data/qddir/demoDataDir-1/ gpdb6-data/dbfast3/demoDataDir2
 
-pg_upgrade --link \
+pg_upgrade  \
 	   --old-bindir=./gpdb5-installation/bin \
 	   --new-bindir=./gpdb6-installation/bin \
 	   --old-datadir=./gpdb5-data/dbfast1/demoDataDir0 \
 	   --new-datadir=./gpdb6-data/dbfast1/demoDataDir0 \
-	   --mode=segment
+	   --mode=segment \
+	   --old-gp-dbid=2 \
+	   --new-gp-dbid=2 \
+	   --old-tablespaces-file=old_tablespaces.txt
 
-pg_upgrade --link \
+pg_upgrade  \
 	   --old-bindir=./gpdb5-installation/bin \
 	   --new-bindir=./gpdb6-installation/bin \
 	   --old-datadir=./gpdb5-data/dbfast2/demoDataDir1 \
 	   --new-datadir=./gpdb6-data/dbfast2/demoDataDir1 \
-	   --mode=segment
+	   --mode=segment \
+	   --old-gp-dbid=3 \
+	   --new-gp-dbid=3 \
+	   --old-tablespaces-file=old_tablespaces.txt
 
-pg_upgrade  --link \
+pg_upgrade  \
 	   --old-bindir=./gpdb5-installation/bin \
 	   --new-bindir=./gpdb6-installation/bin \
 	   --old-datadir=./gpdb5-data/dbfast3/demoDataDir2 \
 	   --new-datadir=./gpdb6-data/dbfast3/demoDataDir2 \
-	   --mode=segment
+	   --mode=segment \
+	   --old-gp-dbid=4 \
+	   --new-gp-dbid=4 \
+	   --old-tablespaces-file=old_tablespaces.txt
 
